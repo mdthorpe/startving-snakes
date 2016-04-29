e7b75ba9c3bd1c2325f9267b4405a7207895b56b
Mike Thorpe <mt@foo.ca>
2016-04-29 16:20:16 -0700

    Why are we using dos line endings?

--- src/preload.js
+++ src/preload.js
@@ -1,14 +1,16 @@
-var preload = function(game){}
+var preload = function(game) {}
 
 preload.prototype = {
-	preload: function(){ 
+    preload: function() {
         this.game.load.spritesheet('balls', 'assets/sprite/balls.png', 17, 17);
         this.game.load.image('body', 'assets/sprite/block.png');
         this.game.load.image('table_bg', 'assets/sprite/table_bg.png');
         this.game.load.physics('tableBounds', 'assets/physics/tableBounds.json');
+        this.game.load.physics('blockOpen', 'assets/physics/blockOpen.json');
+        this.game.load.physics('blockClosed', 'assets/physics/blockClosed.json');
         this.game.stage.backgroundColor = '#124184';
-	},
-  	create: function(){
-		this.game.state.start("TheGame");
-	}
+    },
+    create: function() {
+        this.game.state.start("TheGame");
+    }
 }
--- src/thegame.js
+++ src/thegame.js
@@ -7,6 +7,7 @@ theGame.prototype = {
         this.game.physics.p2.restitution = 0.4;
 
         this.ballsGroup = new Phaser.Group(this.game, '', 'balls', true, false, 'Phaser.Physics.P2JS');
+        this.snakes = [];
 
         // Collision groups
         this.tableCenterCollisionGroup = this.game.physics.p2.createCollisionGroup();
@@ -19,15 +20,19 @@ theGame.prototype = {
 
         // init
         this.players = this.initPlayerList();
+        //  Create a new custom sized bounds, within the world bounds
+        this.customBounds = { left: null, right: null, top: null, bottom: null };
 
         // Add Objects
-        this.addTableBounds(true);
+        this.addTableBounds(false);
         this.addTableCenter(true);
 
-        this.addScoreHole(300, 82, 42, this.players["green"],true);
-        this.addScoreHole(300, 519, 42, this.players["pink"]);
-        this.addScoreHole(82, 299, 42, this.players["orange"]);
-        this.addScoreHole(519, 297, 42, this.players["yellow"]);
+        this.snakes.push(this.addSnake(300, 82, 42, this.players["green"], true));
+
+        this.addScoreHole(300, 82, 42, this.players["green"], true);
+        this.addScoreHole(300, 519, 42, this.players["pink"], true);
+        this.addScoreHole(82, 299, 42, this.players["orange"], true);
+        this.addScoreHole(519, 297, 42, this.players["yellow"], true);
 
         // tmp: Add some test ballsGroup.
         this.game.input.mouse.capture = true;
@@ -41,12 +46,12 @@ theGame.prototype = {
     update: function() {
 
         for (b in this.ballsGroup.children) {
-            var speedToCenter = 5;
+            var speedToCenter = 10;
             if (this.distanceBetween(this.ballsGroup.children[b], this.tableCenter) <= 70) {
                 speedToCenter = 0;
             } else if (this.distanceBetween(this.ballsGroup.children[b], this.tableCenter) <= 100) {
-                speedToCenter = speedToCenter * 5;
-            } else if (this.distanceBetween(this.ballsGroup.children[b], this.tableCenter) < 250) {
+                speedToCenter = speedToCenter * 20;
+            } else if (this.distanceBetween(this.ballsGroup.children[b], this.tableCenter) < 380) {
                 speedToCenter = speedToCenter * 10;
             }
             this.debugCenter = speedToCenter
@@ -56,15 +61,9 @@ theGame.prototype = {
     },
 
     render: function() {
-        // this.game.debug.text(
-        //     this.distanceBetween(this.ballsGroup.children[0],this.tableCenter),
-        //     2, 20, "#ffffff"
-        // );
         this.game.debug.text(this.game.time.fps, 2, 20, "#ffffff")
-        this.game.debug.text(this.debugCenter, 2, 40, "#ffffff");;
-        this.game.debug.text(this.game.input.x, 2, 60, "#ffffff");
-        this.game.debug.text(this.game.input.y, 40, 60, "#ffffff");
-        this.game.debug.text(Phaser.Math.fuzzyFloor(this.game.input.activePointer.duration, 1), 20, 80, "#ffffff");
+        this.game.debug.text(this.game.input.x, 40, 20, "#ffffff");
+        this.game.debug.text(this.game.input.y, 80, 20, "#ffffff");
     },
 
     // Util Functions
@@ -73,13 +72,13 @@ theGame.prototype = {
             speed = 0;
         }
         var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-        obj1.body.force.x = Math.cos(angle) * speed; // accelerateToObject 
+        obj1.body.force.x = Math.cos(angle) * speed; // accelerateToObject
         obj1.body.force.y = Math.sin(angle) * speed;
     },
     distanceBetween: function(spriteA, spriteB) {
-        var dx = spriteA.body.x - spriteB.body.x; //distance ship X to planet X    
-        var dy = spriteA.body.y - spriteB.body.y; //distance ship Y to planet Y    
-        var dist = Math.sqrt(dx * dx + dy * dy); //pythagoras ^^  (get the distance to each other)    
+        var dx = spriteA.body.x - spriteB.body.x; //distance ship X to planet X
+        var dy = spriteA.body.y - spriteB.body.y; //distance ship Y to planet Y
+        var dist = Math.sqrt(dx * dx + dy * dy); //pythagoras ^^  (get the distance to each other)
         return dist;
     },
 
@@ -123,7 +122,6 @@ theGame.prototype = {
         ]);
 
         this.ballsGroup.add(ball);
-
     },
 
 
@@ -132,7 +130,8 @@ theGame.prototype = {
     addTableBounds: function(debug) {
         // Load the table sprite and boundry
         //
-        this.tableBounds = this.game.add.sprite((this.game.width / 2) + 1, (this.game.height / 2) + 1, 'table_bg');
+        this.tableBounds = this.game.add.sprite(
+            (this.game.width / 2) + 1, (this.game.height / 2) + 1, 'table_bg');
 
         this.game.physics.p2.enable(this.tableBounds, debug);
 
@@ -167,7 +166,7 @@ theGame.prototype = {
         // draw a circle
         graphics.lineStyle(1);
         graphics.beginFill(player.color, .15);
-        graphics.drawCircle(x, y, d*2);
+        graphics.drawCircle(x, y, d * 2);
         graphics.endFill();
 
         this.game.physics.p2.enable(hole, debug);
@@ -175,14 +174,63 @@ theGame.prototype = {
         hole.body.player = player;
         hole.body.static = true;
         hole.body.clearShapes();
-        hole.body.addCircle(d/1.5);
+        hole.body.addCircle(d / 1.5);
         hole.body.setCollisionGroup(this.scoreHoleCollisionGroup);
         hole.body.collides([this.ballsCollisionGroup], this.ballInHole, this);
     },
 
-    ballInHole: function(ball,hole) {
-        console.log("ball.player: ", ball.player);
-        this.players[ball.player.name].score += 1;
+    addSnake: function(x, y, r, player, debug) {
+        this.createPreviewBounds(200,200,100,100);
+        //return shapeSprite;
+    },
+
+    ballInHole: function(hole, ball) {
+        this.players[hole.player.name].score += 1;
+        console.log(hole, ball);
+        console.log("hole.player: ", hole.player);
+        ball.sprite.destroy();
+    },
+    createPreviewBounds: function(x, y, w, h) {
+
+        var sim = this.game.physics.p2;
+
+        //  If you want to use your own collision group then set it here and un-comment the lines below
+        var mask = sim.boundsCollisionGroup.mask;
+
+        this.customBounds.left = new p2.Body({
+            mass: 0,
+            position: [sim.pxmi(x), sim.pxmi(y)],
+            angle: 1.5707963267948966
+        });
+        this.customBounds.left.addShape(new p2.Plane());
+
+        this.customBounds.right = new p2.Body({
+            mass: 0,
+            position: [sim.pxmi(x + w), sim.pxmi(y)],
+            angle: -1.5707963267948966
+        });
+        this.customBounds.right.debug = true;
+        this.customBounds.right.addShape(new p2.Plane());
+
+        this.customBounds.top = new p2.Body({
+            mass: 0,
+            position: [sim.pxmi(x), sim.pxmi(y)],
+            angle: -3.141592653589793
+        });
+        this.customBounds.top.addShape(new p2.Plane());
+
+        this.customBounds.bottom = new p2.Body({
+            mass: 0,
+            position: [sim.pxmi(x), sim.pxmi(y + h)]
+        });
+        this.customBounds.bottom.addShape(new p2.Plane());
+
+        sim.world.addBody(this.customBounds.left);
+        sim.world.addBody(this.customBounds.right);
+        sim.world.addBody(this.customBounds.top);
+        sim.world.addBody(this.customBounds.bottom);
+        console.log(this.customBounds);
+
     }
 
 }
